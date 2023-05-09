9412c5bf8e6e8b8824838d5653ef97c000bc6cdf
Len Bekker <len.bekker@sybrin.com>
2023-05-09 14:59:02 +0200

    Renamed generics Added Additional db queries

--- src/services/dbManager.ts
+++ src/services/dbManager.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { aql, Database } from "arangojs";
-import { type AqlQuery } from "arangojs/aql";
+import { join, type AqlQuery, type GeneratedAqlQuery } from "arangojs/aql";
 import * as fs from "fs";
 import NodeCache from "node-cache";
 import { RedisService, type RedisConfig } from "..";
-import { type TransactionRelationship } from "../interfaces";
+import { AccountType, type TransactionRelationship } from "../interfaces";
 
 interface DBConfig {
   url: string;
@@ -42,12 +42,21 @@ interface PseudonymsDB {
    * Note, use "doc." in your query string, as we make use of "doc" as the query and return name.
    * @memberof PseudonymsDB
    */
-  getPseudonymGeneric: (collection: string, filter: string) => Promise<any>;
+  queryPseudonymDB: (collection: string, filter: string, limit?: number) => Promise<any>;
   getPseudonyms: (hash: string) => Promise<any>;
   addAccount: (hash: string) => Promise<any>;
   // addEntity: (entityId: string, CreDtTm: string) => Promise<any>;
   // addAccountHolder: (entityId: string, accountId: string, CreDtTm: string) => Promise<any>;
   saveTransactionRelationship: (tR: TransactionRelationship) => Promise<any>;
+  getPacs008Edge: (endToEndIds: string[]) => Promise<any>;
+  getPacs008Edges: (creditorId: string, threshold: string, amount: number) => Promise<any>;
+  getPacs002Edge: (endToEndIds: string[]) => Promise<any>;
+  getDebtorPacs002Edges: (debtorId: string) => Promise<any>;
+  getSuccessfulPacs002Edges: (creditorId: string, debtorId: string, endToEndId: string) => Promise<any>;
+  getDebtorPacs008Edges: (debtorId: string, endToEndId: string) => Promise<any>;
+  getCreditorPacs008Edges: (creditorId: string) => Promise<any>;
+  getPreviousPacs008Edges: (debtorId: string, limit?: number, to?: string[]) => Promise<any>;
+  getCreditorPacs002Edges: (creditorId: string, threshold: number) => Promise<any>;
 }
 
 interface TransactionHistoryDB {
@@ -67,11 +76,16 @@ interface TransactionHistoryDB {
    * Note, use "doc." in your query string, as we make use of "doc" as the query and return name.
    * @memberof TransactionHistoryDB
    */
-  getTransactionGeneric: (collection: string, filter: string) => Promise<any>;
+  queryTransactionDB: (collection: string, filter: string, limit?: number) => Promise<any>;
   getTransactionPacs008: (endToEndId: string, cacheKey?: string) => Promise<any>;
-  getDebitorPain001Msgs: (creditorId: string) => Promise<any>;
+  getDebtorPain001Msgs: (debtorId: string) => Promise<any>;
   getCreditorPain001Msgs: (creditorId: string) => Promise<any>;
-  getSuccessfulPacs002Msgs: (pain001Id: string) => Promise<any>;
+  getSuccessfulPacs002Msgs: (endToEndId: string) => Promise<any>;
+  getSuccessfulPacs002EndToEndIds: (endToEndIds: string[]) => Promise<any>;
+  getDebtorPacs002Msgs: (endToEndId: string) => Promise<any>;
+  getEquivalentPain001Msg: (endToEndIds: string[]) => Promise<any>;
+  getAccountEndToEndIds: (accountId: string, accountType: AccountType) => Promise<any>;
+  getAccountHistoryPacs008Msgs: (accountId: string, accountType: AccountType) => Promise<any>;
 }
 
 interface ConfigurationDB {
@@ -93,7 +107,7 @@ interface ConfigurationDB {
    * Note, use "doc." in your query string, as we make use of "doc" as the query and return name.
    * @memberof ConfigurationDB
    */
-  getConfigurationGeneric: (collection: string, filter: string) => Promise<any>;
+  queryConfigurationDB: (collection: string, filter: string, limit?: number) => Promise<any>;
   getRuleConfig: (ruleId: string, cfg: string) => Promise<any>;
 }
 
@@ -174,13 +188,15 @@ async function pseudonymsBuilder(manager: DatabaseManagerType, pseudonymsConfig:
     },
   });
 
-  manager.getPseudonymGeneric = async (collection: string, filter: string) => {
+  manager.queryPseudonymDB = async (collection: string, filter: string, limit?: number) => {
     const db = manager._pseudonymsDb!.collection(collection);
     const aqlFilter = aql`${filter}`;
+    const aqlLimit = limit ? aql`LIMIT ${limit}` : null;
 
     const query: AqlQuery = aql`
       FOR doc IN ${db}
       FILTER ${aqlFilter}
+      ${aqlLimit}
       RETURN doc
     `;
 
@@ -222,6 +238,137 @@ async function pseudonymsBuilder(manager: DatabaseManagerType, pseudonymsConfig:
 
     return await manager._pseudonymsDb!.collection("transactionRelationship").save(data, { overwriteMode: "ignore" });
   };
+
+  manager.getPacs008Edge = async (endToEndIds: string[]) => {
+    const db = manager._pseudonymsDb!.collection("transactionRelationship");
+
+    const query = aql`
+      FOR doc IN ${db} 
+      FILTER doc.EndToEndId == IN ${JSON.stringify(endToEndIds)} && doc.TxTp == 'pacs.008.001.10'
+      RETURN doc
+    `;
+
+    return await (await manager._pseudonymsDb!.query(query)).batches.all();
+  };
+
+  manager.getPacs008Edges = async (creditorId: string, threshold?: string, amount?: number) => {
+    const db = manager._pseudonymsDb!.collection("transactionRelationship");
+    const filters: GeneratedAqlQuery[] = [aql`FILTER doc.TxTp == 'pacs.008.001.10' && doc._to == ${creditorId}`];
+
+    if (threshold !== undefined) filters.push(aql`FILTER doc.CreDtTm < ${threshold}`);
+    if (amount !== undefined) filters.push(aql`FILTER doc.Amt == ${amount}`);
+
+    const query = aql`
+      FOR doc IN ${db} 
+      ${join(filters)}
+      RETURN doc
+    `;
+
+    return await (await manager._pseudonymsDb!.query(query)).batches.all();
+  };
+
+  manager.getPacs002Edge = async (endToEndIds: string[]) => {
+    const db = manager._pseudonymsDb!.collection("transactionRelationship");
+
+    const query = aql`
+      FOR doc IN ${db} 
+      FILTER doc.EndToEndId == IN ${JSON.stringify(endToEndIds)} && doc.TxTp == 'pacs.002.001.12'
+      RETURN doc
+    `;
+
+    return await (await manager._pseudonymsDb!.query(query)).batches.all();
+  };
+
+  manager.getDebtorPacs002Edges = async (debtorId: string): Promise<any> => {
+    const db = manager._pseudonymsDb!.collection("transactionRelationship");
+
+    const query = aql`
+      FOR doc IN ${db} 
+      FILTER doc._from == 'accounts/${debtorId}' && doc.TxTp == 'pacs.002.001.12' && doc.TxSts == 'ACCC'
+      RETURN doc
+    `;
+
+    return await (await manager._pseudonymsDb!.query(query)).batches.all();
+  };
+
+  manager.getSuccessfulPacs002Edges = async (creditorId: string, debtorId: string, endToEndId: string): Promise<any> => {
+    const db = manager._pseudonymsDb!.collection("transactionRelationship");
+
+    const query = aql`
+      FOR doc IN ${db} 
+      FILTER doc._to IN ${JSON.stringify(creditorId)}  
+      FILTER doc._from == 'accounts/${debtorId}' 
+      FILTER doc.TxTp == 'pacs.002.001.12' 
+      FILTER doc.EndToEndId IN ${JSON.stringify(endToEndId)}
+      FILTER doc.TxSts == 'ACCC'
+      SORT   doc.CreDtTm DESC
+      LIMIT 2
+      RETURN doc
+    `;
+
+    return await (await manager._pseudonymsDb!.query(query)).batches.all();
+  };
+
+  manager.getDebtorPacs008Edges = async (debtorId: string, endToEndId: string) => {
+    const db = manager._pseudonymsDb!.collection("transactionRelationship");
+
+    const query = aql`
+      FOR doc IN ${db}
+      FILTER doc._from == 'accounts/${debtorId}' 
+      FILTER doc.EndToEndId ==  ${endToEndId}
+      FILTER doc.TxTp == 'pacs.008.001.10' 
+      SORT   doc.CreDtTm DESC
+      LIMIT 2
+      RETURN doc
+    `;
+
+    return await (await manager._pseudonymsDb!.query(query)).batches.all();
+  };
+
+  manager.getCreditorPacs008Edges = async (creditorId: string) => {
+    const db = manager._pseudonymsDb!.collection("transactionRelationship");
+
+    const query = aql`
+      FOR doc IN ${db}
+      FILTER doc._to == 'accounts/${creditorId}' 
+      FILTER doc.TxTp == 'pacs.008.001.10' 
+      SORT   doc.CreDtTm DESC
+      LIMIT 2
+      RETURN doc
+    `;
+
+    return await (await manager._pseudonymsDb!.query(query)).batches.all();
+  };
+
+  manager.getPreviousPacs008Edges = async (accountId: string, limit?: number, to?: string[]) => {
+    const db = manager._pseudonymsDb!.collection("transactionRelationship");
+    const filters: GeneratedAqlQuery[] = [aql`FILTER doc._from == 'accounts/${accountId}' && doc.TxTp == 'pacs.008.001.10'`];
+    if (to !== undefined) filters.push(aql`FILTER doc._to IN ${JSON.stringify(to)}`);
+    const aqlLimit = limit ? aql`${limit}` : aql`3`;
+
+    const query = aql`
+      FOR doc IN ${db}
+      ${join(filters)}
+      SORT doc.CreDtTm DESC
+      LIMIT ${aqlLimit}
+      RETURN doc
+    `;
+
+    return await (await manager._pseudonymsDb!.query(query)).batches.all();
+  };
+
+  manager.getCreditorPacs002Edges = async (creditorId: string, threshold: number) => {
+    const db = manager._pseudonymsDb!.collection("transactionRelationship");
+    const date: string = new Date(new Date().getTime() - threshold).toISOString();
+
+    const query = aql`
+      FOR doc IN ${db}
+      FILTER doc._from == 'accounts/${creditorId}' && doc.TxTp == 'pacs.002.001.12' && doc.TxSts == 'ACCC' && doc.CreDtTm >= ${date}
+        RETURN doc
+    `;
+
+    return await (await manager._pseudonymsDb!.query(query)).batches.all();
+  };
 }
 
 async function transactionHistoryBuilder(manager: DatabaseManagerType, transactionHistoryConfig: DBConfig, redis: boolean): Promise<void> {
@@ -237,13 +384,15 @@ async function transactionHistoryBuilder(manager: DatabaseManagerType, transacti
     },
   });
 
-  manager.getTransactionGeneric = async (collection: string, filter: string) => {
+  manager.queryTransactionDB = async (collection: string, filter: string, limit?: number) => {
     const db = manager._transactionHistory!.collection(collection);
     const aqlFilter = aql`${filter}`;
+    const aqlLimit = limit ? aql`LIMIT ${limit}` : null;
 
     const query: AqlQuery = aql`
       FOR doc IN ${db}
       FILTER ${aqlFilter}
+      ${aqlLimit}
       RETURN doc
     `;
 
@@ -283,7 +432,7 @@ async function transactionHistoryBuilder(manager: DatabaseManagerType, transacti
     };
   }
 
-  manager.getDebitorPain001Msgs = async (creditorId: string) => {
+  manager.getDebtorPain001Msgs = async (creditorId: string) => {
     const db = manager._transactionHistory!.collection("transactionHistoryPain001");
 
     const query: AqlQuery = aql`
@@ -311,15 +460,82 @@ async function transactionHistoryBuilder(manager: DatabaseManagerType, transacti
     return await (await manager._transactionHistory!.query(query)).batches.all();
   };
 
-  manager.getSuccessfulPacs002Msgs = async (pain001Id: string) => {
+  manager.getSuccessfulPacs002Msgs = async (endToEndId: string) => {
     const db = manager._transactionHistory!.collection("transactionHistoryPacs002");
 
     const query: AqlQuery = aql`
       FOR doc IN ${db} 
-      FILTER doc.EndToEndId == ${pain001Id}
+      FILTER doc.EndToEndId == ${endToEndId}
       && doc.TxSts == 'ACCC'
       SORT doc.FIToFIPmtSts.GrpHdr.CreDtTm DESC
       LIMIT 1
+      RETURN doc
+    `;
+
+    return await (await manager._transactionHistory!.query(query)).batches.all();
+  };
+
+  manager.getSuccessfulPacs002EndToEndIds = async (endToEndIds: string[]) => {
+    const db = manager._transactionHistory!.collection("transactionHistoryPacs002");
+
+    const query: AqlQuery = aql`
+      FOR doc IN ${db} 
+      FILTER doc.EndToEndId IN ${JSON.stringify(endToEndIds)}
+      FILTER doc.TxSts == 'ACCC'
+      RETURN doc.EndToEndId
+    `;
+
+    return await (await manager._transactionHistory!.query(query)).batches.all();
+  };
+
+  manager.getDebtorPacs002Msgs = async (endToEndId: string) => {
+    const db = manager._transactionHistory!.collection("transactionHistoryPacs002");
+
+    const query: AqlQuery = aql`
+      FOR doc IN ${db} 
+      FILTER doc.EndToEndId == ${endToEndId}
+      RETURN doc
+    `;
+
+    return await (await manager._transactionHistory!.query(query)).batches.all();
+  };
+
+  manager.getEquivalentPain001Msg = async (endToEndIds: string[]) => {
+    const db = manager._transactionHistory!.collection("transactionHistoryPain001");
+
+    const query: AqlQuery = aql`
+      FOR doc IN ${db} 
+      FILTER doc.EndToEndId IN ${JSON.stringify(endToEndIds)}
+      SORT  doc.EndToEndId DESC 
+      RETURN doc.CstmrCdtTrfInitn.PmtInf.CdtTrfTxInf.RmtInf.Ustrd 
+    `;
+
+    return await (await manager._transactionHistory!.query(query)).batches.all();
+  };
+
+  manager.getAccountEndToEndIds = async (accountId: string, accountType: AccountType) => {
+    const db = manager._transactionHistory!.collection("transactionHistoryPacs008");
+    const accType = accountType === AccountType.CreditorAcct ? aql`CreditorAcctId` : aql`DebtorAcctId`;
+
+    const query: AqlQuery = aql`
+      FOR doc IN ${db} 
+      FILTER doc.${accType} == ${accountId}
+      RETURN { 
+        e2eId: doc.EndToEndId,
+        timestamp: DATE_TIMESTAMP(doc.CreDtTm) 
+      }
+    `;
+
+    return await (await manager._transactionHistory!.query(query)).batches.all();
+  };
+
+  manager.getAccountHistoryPacs008Msgs = async (accountId: string, accountType: AccountType) => {
+    const db = manager._transactionHistory!.collection("transactionHistoryPacs008");
+    const accType = accountType === AccountType.CreditorAcct ? aql`CreditorAcctId` : aql`DebtorAcctId`;
+
+    const query: AqlQuery = aql`
+      FOR doc IN ${db} 
+      FILTER doc.${accType} == ${accountId}
       RETURN doc
     `;
 
@@ -343,13 +559,15 @@ async function configurationBuilder(manager: DatabaseManagerType, configurationC
   manager.setupConfig = configurationConfig;
   manager.nodeCache = new NodeCache();
 
-  manager.getConfigurationGeneric = async (collection: string, filter: string) => {
+  manager.queryConfigurationDB = async (collection: string, filter: string, limit?: number) => {
     const db = manager._configuration!.collection(collection);
     const aqlFilter = aql`${filter}`;
+    const aqlLimit = limit ? aql`LIMIT ${limit}` : null;
 
     const query: AqlQuery = aql`
       FOR doc IN ${db}
       FILTER ${aqlFilter}
+      ${aqlLimit}
       RETURN doc
     `;
 
@@ -370,10 +588,10 @@ async function configurationBuilder(manager: DatabaseManagerType, configurationC
       RETURN doc
     `;
 
-    const toReturn = (await manager._configuration!.query(query)).batches.all();
+    const toReturn = await (await manager._configuration!.query(query)).batches.all();
     if (manager.setupConfig?.localCacheEnabled) manager.nodeCache?.set(cacheKey, toReturn, manager.setupConfig?.localCacheTTL ?? 3000);
 
-    return await toReturn;
+    return toReturn;
   };
 }
 
