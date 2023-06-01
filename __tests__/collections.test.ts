9427de20e9749bdd50ab86a67a7c03af1c1f9b92
kawaki-san <rodney.kanjala@sybrin.com>
2023-06-01 11:28:06 +0200

    test: add new collection name tests

--- __tests__/collections.test.ts
+++ __tests__/collections.test.ts
@@ -1,29 +1,41 @@
-import { arangoPseudonyms, arangoTransactions } from '../src/interfaces/ArangoCollections';
+import { dbConfiguration, dbNetworkMap, dbPseudonyms, dbTransactions } from '../src/interfaces/ArangoCollections';
 
 describe('Should point to correct collections in transactionHistory', () => {
   test('transactionHistoryPacs008', () => {
-    expect(arangoTransactions.pacs008).toBe('transactionHistoryPacs008');
+    expect(dbTransactions.pacs008).toBe('transactionHistoryPacs008');
   });
 
   test('transactionHistoryPacs002', () => {
-    expect(arangoTransactions.pacs002).toBe('transactionHistoryPacs002');
+    expect(dbTransactions.pacs002).toBe('transactionHistoryPacs002');
   });
 
   test('transactionHistoryPain001', () => {
-    expect(arangoTransactions.pain001).toBe('transactionHistoryPain001');
+    expect(dbTransactions.pain001).toBe('transactionHistoryPain001');
   });
 });
 
 describe('Should point to correct collections in pseudonyms', () => {
   test('pseudonyms', () => {
-    expect(arangoPseudonyms.self).toBe('pseudonyms');
+    expect(dbPseudonyms.self).toBe('pseudonyms');
   });
 
   test('transactionRelationship', () => {
-    expect(arangoPseudonyms.edges).toBe('transactionRelationship');
+    expect(dbPseudonyms.edges).toBe('transactionRelationship');
   });
 
   test('accounts', () => {
-    expect(arangoPseudonyms.accounts).toBe('accounts');
+    expect(dbPseudonyms.accounts).toBe('accounts');
+  });
+});
+
+describe('Should point to correct collections in configuration', () => {
+  test('configuration', () => {
+    expect(dbConfiguration.self).toBe('configuration');
+  });
+});
+
+describe('Should point to correct collections in networkmap', () => {
+  test('networkConfiguration', () => {
+    expect(dbNetworkMap.netConfig).toBe('networkConfiguration');
   });
 });
