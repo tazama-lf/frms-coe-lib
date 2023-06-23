83fd6bd80023be8499a62daf779e48455b35874a
kawaki-san <rodney.kanjala@sybrin.com>
2023-06-23 11:28:36 +0200

    docs: unwrap utility fn

--- src/helpers/unwrap.ts
+++ src/helpers/unwrap.ts
@@ -1,3 +1,13 @@
+/**
+ * Takes a generic 2D array and returns the item at index [0][0] if it's not loosely equal to null
+ *
+ * @remarks
+ * This function's intention is to 'unwrap' results coming from calls made by this library.
+ *
+ * @param type - A generic two dimensional array
+ * @returns The item at [0][0] or undefined if and the indexed item is loosely equal to null
+ *
+ */
 export const unwrap = <T>(type: T[][]): T | undefined => {
   if (type[0][0] != null) {
     return type[0][0];
