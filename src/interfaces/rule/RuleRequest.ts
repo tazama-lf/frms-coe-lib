216361fe0b9a8f54eb0d18ad7042e1df68e85d9a
cshezi <cshezi@sybrin.co.za>
2023-07-18 16:22:15 +0200

    fix: adding metadata type

--- src/interfaces/rule/RuleRequest.ts
+++ src/interfaces/rule/RuleRequest.ts
@@ -2,8 +2,14 @@ import { type Pacs002 } from '../Pacs.002.001.12';
 import { type NetworkMap } from '../NetworkMap';
 import { type DataCache } from '..';
 
+interface MetaData {
+  prcgTmDp: number;
+  prcgTmCRSP: number;
+}
+
 export interface RuleRequest {
   transaction: Pacs002;
   networkMap: NetworkMap;
   DataCache: DataCache;
+  metaData?: MetaData;
 }
