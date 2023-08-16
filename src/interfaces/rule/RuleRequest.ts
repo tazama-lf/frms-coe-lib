0c80407f8ebdd949f98c607614b77604aa5aec0c
rtkay123 <rodney.kanjala@sybrin.com>
2023-08-16 12:22:46 +0200

    refactor: make metaData allow for arbitrary fields & types

--- src/interfaces/rule/RuleRequest.ts
+++ src/interfaces/rule/RuleRequest.ts
@@ -2,14 +2,9 @@ import { type Pacs002 } from '../Pacs.002.001.12';
 import { type NetworkMap } from '../NetworkMap';
 import { type DataCache } from '..';
 
-interface MetaData {
-  prcgTmDp: number;
-  prcgTmCRSP: number;
-}
-
 export interface RuleRequest {
   transaction: Pacs002;
   networkMap: NetworkMap;
   DataCache: DataCache;
-  metaData?: MetaData;
+  metaData?: Record<string, unknown>;
 }
