b830ea4dd0e77ab2c86808ba03e885d997789813
Len Bekker <len.bekker@sybrin.com>
2023-09-01 12:46:00 +0200

    feat: add classes and update/made compatible

--- src/interfaces/processor-files/Alert.ts
+++ src/interfaces/processor-files/Alert.ts
@@ -1,8 +1,10 @@
 import { v4 } from 'uuid';
 import { TADPResult } from './TADPResult';
+import { MetaData } from '../metaData';
 
 export class Alert {
   evaluationID = v4();
+  metaData? = new MetaData();
   status = ''; // eg ALRT
   timestamp: Date = new Date();
   tadpResult: TADPResult = new TADPResult();
