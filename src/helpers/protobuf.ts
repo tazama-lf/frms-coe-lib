ebd58fcf1bf2dd1903c41fb6740c20622415656e
rtkay123 <dev@kanjala.com>
2023-09-15 10:44:02 +0200

    add: helper fn to create `Buffer`

--- src/helpers/protobuf.ts
+++ src/helpers/protobuf.ts
@@ -2,4 +2,21 @@ import protobuf from 'protobufjs';
 import path from 'node:path';
 const root = protobuf.loadSync(path.join(__dirname, '/proto/Full.proto'));
 const FRMSMessage = root.lookupType('FRMSMessage');
+
+/**
+ * Create a `Buffer` derived from a byte array resulting from the input type
+ *
+ * @param {Record<string, unknown>} data The object to serialise to a `Buffer`
+ * @returns {Buffer | undefined} The resulting `Buffer`, or `undefined` if an error occured
+ */
+export const createMessageBuffer = (data: Record<string, unknown>): Buffer | undefined => {
+  try {
+    const msg = FRMSMessage.create(data);
+    const enc = FRMSMessage.encode(msg).finish();
+    return Buffer.from(enc);
+  } catch (error) {
+    return undefined;
+  }
+};
+
 export default FRMSMessage;
