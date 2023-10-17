8a262f999fc3b067aa91c85258d4c8c3201ee03a
Jean-Pierre <Jean-Pierre.NelL@Sybrin.com>
2023-10-17 14:17:18 +0200

    bug: removed threshold

--- src/interfaces/processor-files/TypologyResult.ts
+++ src/interfaces/processor-files/TypologyResult.ts
@@ -5,7 +5,6 @@ export class TypologyResult {
   cfg = '';
   prcgTm? = 0;
   result = 0.0;
-  threshold = 0.0;
   review? = false;
   ruleResults: RuleResult[] = [];
   workflow: WorkFlow = new WorkFlow();
