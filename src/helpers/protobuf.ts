a4bfe8037480773806ba88913ed1784814645178
rtkay123 <rodney.kanjala@sybrin.com>
2023-11-28 10:33:51 +0200

    chore: bump lib ver

--- src/helpers/protobuf.ts
+++ src/helpers/protobuf.ts
@@ -1,10 +1,14 @@
 import protobuf from 'protobufjs';
 import path from 'node:path';
+import { type LogMessage as LogMessageType } from './proto/message/LogMessage';
 const root = protobuf.loadSync(path.join(__dirname, '/proto/Full.proto'));
 const FRMSMessage = root.lookupType('FRMSMessage');
 
+const log = protobuf.loadSync(path.join(__dirname, '/proto/Lumberjack.proto'));
+const LogMessage = log.lookupType('LogMessage');
+
 /**
- * Create a `Buffer` derived from a byte array resulting from the input type
+ * Create a Message `Buffer` derived from a byte array resulting from the input type
  *
  * @param {Record<string, unknown>} data The object to serialise to a `Buffer`
  * @returns {Buffer | undefined} The resulting `Buffer`, or `undefined` if an error occured
@@ -17,6 +21,33 @@ export const createMessageBuffer = (data: Record<string, unknown>): Buffer | und
   } catch (error) {
     return undefined;
   }
+};
+
+/**
+ * Create a Log `Buffer` derived from a byte array resulting from the input type
+ *
+ * @param {Record<string, unknown>} data The object to serialise to a `Buffer`
+ * @returns {Buffer | undefined} The resulting `Buffer`, or `undefined` if an error occured
+ */
+export const createLogBuffer = (data: Record<string, unknown>): Buffer | undefined => {
+  try {
+    const msg = LogMessage.create(data);
+    const enc = LogMessage.encode(msg).finish() as Buffer;
+    return enc;
+  } catch (error) {
+    return undefined;
+  }
+};
+
+/**
+ * Decodes a Log `Buffer` derived from a byte array resulting in a concrete `LogMessage` type
+ *
+ * @param {Buffer} buffer The byte array to decode to a `LogMessage`
+ * @returns {LogMessage | undefined} The resulting `LogMessage`, or `undefined` if an error occured
+ */
+export const decodeLogBuffer = (buffer: Buffer): LogMessageType | undefined => {
+  const decodedMessage = LogMessage.decode(buffer);
+  return LogMessage.toObject(decodedMessage);
 };
 
 export default FRMSMessage;
