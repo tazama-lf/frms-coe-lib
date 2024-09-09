981b2499b26f5f52cf33dd41a17066fa54670dc0
rtkay123 <rodney.kanjala@sybrin.com>
2024-09-09 13:04:49 +0200

    test: encode/decode conditions

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -1,55 +1,61 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import { createConditionsBuffer, decodeConditionsBuffer } from '../src/helpers/protobuf';
+import {
+  createCacheConditionsBuffer,
+  createConditionsBuffer,
+  decodeCacheConditionsBuffer,
+  decodeConditionsBuffer,
+} from '../src/helpers/protobuf';
 import { Acct } from '../src/interfaces/event-flow/EntityConditionEdge';
 
-describe('Should point to correct collections in transactionHistory', () => {
-  test('se/deserialise EFRuP conditions', () => {
-    const acct: Acct = {
-      id: '1010101010',
-      schmeNm: {
-        prtry: 'Mxx',
+const acct: Acct = {
+  id: '1010101010',
+  schmeNm: {
+    prtry: 'Mxx',
+  },
+  agt: {
+    finInstnId: {
+      clrSysMmbId: {
+        mmbId: 'dfsp001',
       },
-      agt: {
-        finInstnId: {
-          clrSysMmbId: {
-            mmbId: 'dfsp001',
-          },
-        },
-      },
-    };
-    const conditions = [
+    },
+  },
+};
+const conditions = [
+  {
+    condId: '26819',
+    condTp: 'non-overridable-block',
+    incptnDtTm: '2024-09-01T24:00:00.999Z',
+    xprtnDtTm: '2024-09-03T24:00:00.999Z',
+    condRsn: 'R001',
+    usr: 'bob',
+    creDtTm: '2024-08-23T11:46:53.091Z',
+    prsptvs: [
       {
-        condId: '26819',
-        condTp: 'non-overridable-block',
+        prsptv: 'governed_as_creditor_by',
+        evtTp: ['pacs.008.001.10', 'pacs.002.001.12'],
         incptnDtTm: '2024-09-01T24:00:00.999Z',
         xprtnDtTm: '2024-09-03T24:00:00.999Z',
-        condRsn: 'R001',
-        usr: 'bob',
-        creDtTm: '2024-08-23T11:46:53.091Z',
-        prsptvs: [
-          {
-            prsptv: 'governed_as_creditor_by',
-            evtTp: ['pacs.008.001.10', 'pacs.002.001.12'],
-            incptnDtTm: '2024-09-01T24:00:00.999Z',
-            xprtnDtTm: '2024-09-03T24:00:00.999Z',
-          },
-          {
-            prsptv: 'governed_as_debtor_by',
-            evtTp: ['pacs.008.001.10', 'pacs.002.001.12'],
-            incptnDtTm: '2024-09-01T24:00:00.999Z',
-            xprtnDtTm: '2024-09-03T24:00:00.999Z',
-          },
-        ],
       },
-    ];
+      {
+        prsptv: 'governed_as_debtor_by',
+        evtTp: ['pacs.008.001.10', 'pacs.002.001.12'],
+        incptnDtTm: '2024-09-01T24:00:00.999Z',
+        xprtnDtTm: '2024-09-03T24:00:00.999Z',
+      },
+    ],
+  },
+];
 
-    const entity = {
-      id: '+27733161225',
-      schmeNm: {
-        prtry: 'MSISDN',
-      },
-    };
+const entity = {
+  id: '+27733161225',
+  schmeNm: {
+    prtry: 'MSISDN',
+  },
+};
+
+describe('Should serialise/deserialise EFRuP conditions', () => {
+  test('se/deserialise EFRuP conditions', () => {
     const accountConditions = { acct: acct, conditions };
     const bufAcc = createConditionsBuffer(accountConditions);
     expect(bufAcc).toBeDefined();
@@ -65,5 +71,18 @@ describe('Should point to correct collections in transactionHistory', () => {
     const deserialisedEntityConditions = decodeConditionsBuffer(bufEntity!);
     expect(deserialisedEntityConditions).toBeDefined();
     expect(deserialisedEntityConditions).toEqual(entityConditons);
+  });
+
+  test('se/deserialise ALL EFRuP conditions', () => {
+    const accountConditions = { acct: acct, conditions };
+    const entityConditons = { ntty: entity, conditions };
+    let conds = { account: accountConditions, entity: entityConditons };
+    const bufConds = createCacheConditionsBuffer(conds);
+    expect(bufConds).toBeDefined();
+    expect(bufConds?.byteLength).toBeGreaterThan(0);
+
+    const deserialisedConditions = decodeCacheConditionsBuffer(bufConds!);
+    expect(deserialisedConditions).toBeDefined();
+    expect(deserialisedConditions).toEqual(conds);
   });
 });
