8b52b9d93df070a060ab6328153cb6b8ac32e40b
Len Bekker <len.bekker@sybrin.com>
2025-08-29 12:03:56 +0200

    tests: added some prototype mocks for rules
    written by Nel

--- src/tests/data/networkMap.ts
+++ src/tests/data/networkMap.ts
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import { NetworkMap } from '../../interfaces';
+import type { NetworkMap } from '../../interfaces';
 
 export const NetworkMapSample: NetworkMap[] = [
   {
