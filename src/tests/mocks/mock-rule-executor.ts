d8814f9639c30986dad0e4c8b5a8b39bc631b67e
Justus-at-Tazama <jortlepp@contractor.linuxfoundation.org>
2026-02-03 09:27:42 +0200

    fix: integrated independent variable with the determineOutcome mock function for testing

--- src/tests/mocks/mock-rule-executor.ts
+++ src/tests/mocks/mock-rule-executor.ts
@@ -3,6 +3,7 @@ import type { RuleConfig, RuleResult } from '../../interfaces';
 export function determineOutcome(value: number, ruleConfig: RuleConfig, ruleResult: RuleResult): RuleResult {
   const { bands } = ruleConfig.config;
   if (bands && (value || value === 0)) {
+    ruleResult.indpdntVarbl = value;
     for (const band of bands) {
       if ((!band.lowerLimit || value >= band.lowerLimit) && (!band.upperLimit || value < band.upperLimit)) {
         ruleResult.subRuleRef = band.subRuleRef;
