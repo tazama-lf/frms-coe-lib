141bdca5f9605e46d6d33d4ac8c33b3852f54c39
fatimali03 <fatima.ali@paysyslabs.com>
2026-02-27 10:23:55 +0500

    fix: transaction to unknown

--- src/interfaces/rule/RuleRequest.ts
+++ src/interfaces/rule/RuleRequest.ts
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import type { Pacs002 } from '../Pacs.002.001.12';
 import type { NetworkMap } from '../NetworkMap';
 import type { DataCache } from '..';
 import type { MetaData } from '../metaData';
 
 export interface RuleRequest {
-  transaction: Pacs002;
+  transaction: unknown;
   networkMap: NetworkMap;
   DataCache: DataCache;
   metaData?: MetaData;
