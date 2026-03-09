f16d9e4a456d1e2a4d54545983a67c0dafe9e862
abdul-rahim-psl <abdul.rahim@paysyslabs.com>
2026-03-09 16:11:32 +0500

    fix: changes acording to coderabbit comments

--- src/interfaces/DEMS/TrackedFields.ts
+++ src/interfaces/DEMS/TrackedFields.ts
@@ -1,8 +1,8 @@
 export interface TrackedFields {
-  CreDtTm: string;
-  MsgId: string;
-  EndToEndId: string;
-  dbtrAcctId: string | null;
-  cdtrAcctId: string | null;
-  TenantId: string;
+  CreDtTm?: string;
+  MsgId?: string;
+  EndToEndId?: string;
+  dbtrAcctId?: string | null;
+  cdtrAcctId?: string | null;
+  TenantId?: string;
 }
