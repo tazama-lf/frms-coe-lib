b29bdfbb54adaf0c83b0a0829b750c4886cba0ba
ReebaPaysys <reeba.siddiqui@paysyslabs.com>
2026-04-08 17:44:57 +0500

    fix: handle rulerequest

--- src/interfaces/rule/RuleRequest.ts
+++ src/interfaces/rule/RuleRequest.ts
@@ -1,14 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { NetworkMap } from '../NetworkMap';
-import type { DataCache } from '..';
+import type { DataCache, SupportedTransactionMessage } from '..';
 import type { MetaData } from '../metaData';
-import type { SupportedTransactionMessage } from '../BaseMessage';
-
-export interface RuleRequest {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any -- Transaction type varies based on payment message format
-  //transaction: any;
-  transaction: SupportedTransactionMessage;
+export interface RuleRequest<T extends SupportedTransactionMessage = SupportedTransactionMessage> {
+  transaction: T;
   networkMap: NetworkMap;
   DataCache: DataCache;
   metaData?: MetaData;
